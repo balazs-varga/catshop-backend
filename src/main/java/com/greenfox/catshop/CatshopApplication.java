1d9fb1adf3d51cd0da1c12c3cb13c51c49a6f2f2
balazs-varga <balazsvarga91@gmail.com>
2018-07-03 10:43:38 +0200

    feat(master): created project

---
+++ src/main/java/com/greenfox/catshop/CatshopApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.catshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatshopApplication.class, args);
+	}
+}
