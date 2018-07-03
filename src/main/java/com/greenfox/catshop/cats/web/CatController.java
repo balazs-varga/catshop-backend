0ff89a5b9924b3e36fcab2ef77f46c2fd0ff3299
baloghdominik <balogh0dominik@gmail.com>
2018-07-03 14:33:25 +0200

    created get api/cats/id

--- src/main/java/com/greenfox/catshop/cats/web/CatController.java
+++ src/main/java/com/greenfox/catshop/cats/web/CatController.java
@@ -1,21 +1,38 @@
 package com.greenfox.catshop.cats.web;
 
+import com.greenfox.catshop.cats.error.CatNotFoundException;
 import com.greenfox.catshop.cats.model.CatDTO;
 import com.greenfox.catshop.cats.service.CatService;
+import com.greenfox.catshop.error.ErrorResource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CatController {
 
   @Autowired
   CatService catService;
 
-  @GetMapping({"/api/cats"})
+  @GetMapping("/cats")
   public List<CatDTO> listCats() {
     return catService.listCats();
+  }
+
+  @GetMapping("/cats/{id}")
+  public CatDTO getCat(@PathVariable("id") Long id) {
+    return catService.getCat(id);
+  }
+
+  @ExceptionHandler({CatNotFoundException.class})
+  public ErrorResource handlePermissionException(HttpServletResponse response) {
+    response.setStatus(403);
+    return new ErrorResource(
+            "Cat not found.",
+            HttpStatus.BAD_REQUEST);
   }
 }
