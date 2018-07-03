9d6d76da66d9fa6abadf55ca57d43bd76e2182e2
balazs-varga <balazsvarga91@gmail.com>
2018-07-03 16:36:43 +0200

    refact: fixed fluffinessQuery method

--- src/main/java/com/greenfox/catshop/cats/web/CatController.java
+++ src/main/java/com/greenfox/catshop/cats/web/CatController.java
@@ -21,11 +21,6 @@ public class CatController {
   @Autowired
   CatService catService;
 
-  @GetMapping("/cats")
-  public List<CatDTO> listCats() {
-    return catService.listCats();
-  }
-
   @GetMapping({"/cats"})
   public List<CatDTO> fluffinessQuery(@RequestParam(name = "fluffiness", required = false) String fluffiness) {
     if (fluffiness == null) {
